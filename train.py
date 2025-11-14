3365ce1e581c1e5d02420a153c8191d6992d6eaa
Đồng Minh Đức <dongminhduc1903@gmail.com>
2025-11-15 00:34:13 +0700

    temp submit (struggling with loading model)

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ from ultralytics import YOLOv10
 config_path = './config.yaml'
 
 #load model 
-model = YOLOv10.from_pretrained('jameslahm/yolov10n')
+
+model = YOLOv10("yolov10n.pt")
+
 #train model
 model.train(data=config_path, epochs=200, batch=32)
